36fcd8225093e220afbe60bda7c93b663383b82e
Виктория Захарова <zaharova181114@gmail.com>
2023-04-21 11:54:53 +0300

    add page catalog

--- js/common.js
+++ js/common.js
@@ -6,3 +6,27 @@ $(function () {
 			.closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
 	});
 });
+
+// menu sidebar
+// $('.dropdown-toggle').click(function (e) {
+// 	e.preventDefault();
+// 	if ($('.dropdown-toggle').hasClass('open')) {
+// 		$('.open').siblings('.dropdown-menu').css('display', 'block');
+// 	}
+// 	$(this).toggleClass('open').siblings('.dropdown-menu').slideToggle();
+// });
+
+function accordeon() {
+	var panel = $('.dropdown');
+
+	if (panel.hasClass('open')) {
+		$('.open').find('.dropdown-menu').slideDown();
+	}
+
+	$('.dropdown-toggle').on('click', function (e) {
+		e.preventDefault();
+		$(this).parent().toggleClass('open').find('.dropdown-menu').slideToggle();
+	});
+}
+
+accordeon();
